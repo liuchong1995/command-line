01cbc044bda3bc315fc95434672b506765a377e5
liuchong1995 <792043775@qq.com>
2019-03-09 14:04:13 +0800

    javap

---
+++ src/org/home/bytecode/TTTT.java
@@ -0,0 +1,19 @@
+package org.home.bytecode;
+
+/**
+ * @Author: LiuChong
+ * @Date: 2019-03-09 13:54
+ * @Description:
+ */
+public class TTTT {
+
+    public static void main(String[] args) {
+        int i = 0;
+        System.out.println(i++);
+    }
+
+    public void tt1(){
+        int i = 0;
+        System.out.println(++i);
+    }
+}
